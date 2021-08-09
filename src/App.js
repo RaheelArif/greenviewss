7f64233e6fb53d570078fb91f464d41c1d719031
Raheel Arif <raheelarif29@gmail.com>
2021-08-09 17:59:21 +0500

    sue state and set state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
 import Header from "./components/Header";
 import Header2 from "./components/Header2";
 import Content from "./components/Content";
 import "./App.css";
 
 function App() {
-  const [header, setHeader] = useState("green");
 
-  const changeHeader = () => {
-    setHeader("green view");
-  };
   return (
     <div>
       <Header />
       <Header2 />
-      <button onClick={changeHeader}>change name</button>
-      <p>{header}</p>
       <Content />
     </div>
   );
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,43 @@
+import { useState } from "react";
+
 function Content() {
-  var a = 2;
-  a = a + 5;
+  const [sName, setName] = useState("");
+  const [sRollN, setRollN] = useState("");
+  const [students, setStudents] = useState([
+    { name: "ali", rollN: 1 },
+    { name: "ahmed", rollN: 2 },
+  ]);
+
+  const saveStudent = () => {
+    let student = {name: sName , rollN: sRollN}
+    let newStudents = [...students]
+    newStudents.push(student)
+    setStudents(newStudents)
+  };
   return (
-    <div>
-      <h1 style={{ color: "red" }}>gggg</h1>
-      <h2 className="heading">heading 2 </h2>
-      <div>
-        <ol>
-          <li style={{ backgroundColor: "green", color: "white" }}>a</li>
-          <li>{a}</li>
-          <li>c</li>
-          <li>g</li>
-          <li>k</li>
-        </ol>
+    <div className="content-main-c">
+      {students.map((s, i) => {
+        return (
+          <div key={i} className="s-div-c">
+            <h3>student name {s.name} </h3>
+            <p>roll number is {s.rollN}</p>
+          </div>
+        );
+      })}
+      <div className="s-div-c">
+        <input
+          value={sName}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="student name"
+          type="text"
+        />
+        <input
+          value={sRollN}
+          onChange={(e) => setRollN(e.target.value)}
+          placeholder="roll number"
+          type="text"
+        />
+        <button onClick={saveStudent}>save</button>
       </div>
     </div>
   );
