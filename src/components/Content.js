295df3b14198b4612132fbb414b9c91ef7c721dc
Raheel Arif <raheelarif29@gmail.com>
2021-08-10 17:37:30 +0500

    delete funtion

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,10 +9,23 @@ function Content() {
   ]);
 
   const saveStudent = () => {
-    let student = {name: sName , rollN: sRollN}
-    let newStudents = [...students]
-    newStudents.push(student)
-    setStudents(newStudents)
+    let student = { name: sName, rollN: sRollN };
+    let newStudents = [...students];
+    newStudents.push(student);
+    setStudents(newStudents);
+  };
+  const handleDelete = (student) => {
+    let oldStunts = [...students];
+    let filteredStudents = oldStunts.filter((s) => {
+     return s.rollN !== student.rollN;
+    });
+    setStudents(filteredStudents);
+    // let filteredStudents = [];
+    // for (let i = 0; i < oldStunts.length; i++) {
+    //   if (oldStunts[i].rollN !== student.rollN) {
+    //     filteredStudents.push(oldStunts[i])
+    //   }
+    // }
   };
   return (
     <div className="content-main-c">
@@ -21,6 +34,7 @@ function Content() {
           <div key={i} className="s-div-c">
             <h3>student name {s.name} </h3>
             <p>roll number is {s.rollN}</p>
+            <button onClick={() => handleDelete(s)}>delete</button>
           </div>
         );
       })}
